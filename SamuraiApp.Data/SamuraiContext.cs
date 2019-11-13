4d48d8c2c68733a940cc0460dbbed96355aa8fd3
marcus quigley <marcusquigley@gmail.com>
2019-11-13 10:49:07 -0500

    adding DBcontext

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Data
+{
+   public class SamuraiContext: DbContext
+    {
+        readonly string _connectionString = "Server = (localdb)\\mssqllocaldb; Database=SamuraiAppData;Trusted_Connection = True;";
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quote { get; set; }
+        public DbSet<Battle> Battle { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
